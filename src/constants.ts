aaef1671a5262187d323f9882297a9fea1992951
Florian Klampfer <mail@qwtel.com>
2021-03-14 10:58:30 +0700

    Commit

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-import { StorageArea } from '@worker-tools/kv-storage';
+// import { StorageArea } from '@worker-tools/kv-storage';
 
-export const IP_SALT_KEY = 'IP_SALT';
-export const NAMESPACE = 'c4e75796-9fe6-ce66-612e-534b709074ef';
+// export const IP_SALT_KEY = 'IP_SALT';
+// export const NAMESPACE = 'c4e75796-9fe6-ce66-612e-534b709074ef';
 
-export const storage = new StorageArea();
+// export const storage = new StorageArea();
