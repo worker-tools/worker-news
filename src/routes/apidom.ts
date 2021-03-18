86a5e57b9d2a31325941bc3be1459975f5d893eb
Florian Klampfer <mail@qwtel.com>
2021-03-18 10:50:34 +0700

    Commit

--- src/routes/apidom.ts
+++ src/routes/apidom.ts
@@ -3,64 +3,69 @@ import { parseHTML, DOMParser } from 'linkedom';
 
 const from = Array.from.bind(Array);
 
-export function* stories(body: string) {
-  if (!/[<>]/.test(body)) {
-    throw new Error('Not HTML content');
-  } else {
-    const { document } = parseHTML(body);
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
-
-    // const posts = [];
-    const nodeList = has($$('td table'), 'td.title')[0]?.querySelectorAll('tr');
-    const rows = from(nodeList).filter(r => r.querySelector('td.title, td.subtext'));
-
-    for (let i = 0, l = rows.length; i < l; i += 2) {
-      const row1 = rows[i];
-      const row2 = rows[i + 1];
-      if (!row2.children) break;
-
-      const voteLink = row1.querySelector('td a[id^=up]');
-      let id = (voteLink ? (voteLink.getAttribute('id')?.match(/\d+/) || [])[0] : null);
-
-      const cell1 = row1.querySelector('td.title');
-      const link = cell1?.querySelector<HTMLAnchorElement>('a:first-of-type');
-      const title = link?.textContent?.trim();
-      const url = link?.getAttribute('href');
-      const domain = url && tryURL(url)?.origin;
-
-      const cell2 = row2.querySelector('td.subtext');
-      const points = Number(cell2?.querySelector('span[id^=score]')?.textContent);
-      const userLink = cell2?.querySelector('a[href^=user]');
-      const user = userLink?.textContent || null;
-      const postLinks = cell2?.querySelectorAll('a[href^=item]');
-      const timeAgoLink = postLinks?.[0];
-      let time_ago = timeAgoLink?.textContent?.trim();
-      const commentsCountLink = postLinks?.[1];
-      const comments_count = commentsCountLink && /\d/.test(commentsCountLink.textContent || '')
-        ? Number(commentsCountLink.textContent)
-        : 0;
-
-      let type = 'link';
-      if (url?.match(/^item/i)) type = 'ask';
-      if (!user) { // No users post this = job ads
-        type = 'job';
-        id = (url?.match(/\d+/) || [])[0];
-        time_ago = cell2?.innerHTML.trim();
-      }
-
-      yield { id, title, url, domain, points, user, time_ago, comments_count, type }
-    }
+const tryURL = (url: string): URL | null => {
+  try {
+    return new URL(url);
+  } catch {
+    return null;
   }
 }
 
-function cleanContent(html: string) {
-  // yea yea regex to clean HTML is lame yada yada
-  html = html.replace(/">-+<\/font/ig, '"></font'); // remove weird invisible dashes at the end of comments
-  html = html.replace(/<\/?font[^<>]*>/ig, ''); // remove font tags
-  html = html.replace(/<\/p>/ig, ''); // remove trailing </p>s
-  if (!html.match(/^<p>/i)) html = '<p>' + html; // prepend <p>
-  return html;
+async function consume(r: Response) {
+  const reader = r.body!.getReader()
+  while (!(await reader.read()).done);
+}
+
+const API = 'https://news.ycombinator.com'
+
+async function getStories(p = 1): Promise<Post[]> {
+  try {
+    const url = new ParamsURL('/news', { p }, API).href;
+    const body = await fetch(url);
+    return stories(body);
+  } catch (err) {
+    console.error(err)
+    throw err;
+  }
+}
+
+export { getStories as stories }
+
+async function stories(response: Response) {
+  const posts: Partial<Post>[] = [];
+
+  await consume(new HTMLRewriter()
+    .on('.athing[id]', {
+      element(el) {
+        const id = Number(el.getAttribute('id'))
+        console.log(id)
+        posts.unshift({ id, title: '', user: '', time_ago: '' });
+      }
+    })
+    .on('.athing[id] .title a.storylink', {
+      element(link) {
+        const url = posts[0].url = link?.getAttribute('href') || undefined;
+        posts[0].domain = url && tryURL(url)?.hostname;
+      },
+      text({ text }) { posts[0].title += text },
+    })
+    // // FIXME: concatenate text before parseInt jtbs..
+    .on('.subtext > .score', { text({ text }) { posts[0].points ||= parseInt(text, 10) } })
+    .on('.subtext > .hnuser', { text({ text }) { posts[0].user += text } })
+    .on('.subtext > .age', { text({ text }) { posts[0].time_ago += text } })
+    .on('.subtext > a[href^=item]', { text({ text }) { posts[0].comments_count ||= parseInt(text, 10) } })
+    .transform(response));
+
+  const postsX = posts.map(post => {
+    post.type = 'story';
+    if (post.url?.match(/^item/i)) post.type = 'ask';
+    if (!post.user) { // No users post this = job ads
+      post.type = 'job';
+    }
+    return post as Post;
+  })
+
+  return postsX;
 }
 
 export interface Comment {
@@ -74,115 +79,11 @@ export interface Comment {
   _stack: string[]
 }
 
-function processComments(rows: Element[]) {
-  let comments: Comment[] = [];
-
-  // Create flat array of comments
-  for (let i = 0, l = rows.length; i < l; i++) {
-    const row = rows[i];
-    let level = 0;
-    const levelRow = row.querySelector('img[src*="s.gif"]');
-    const metadata = from(row.querySelectorAll('.comhead')).filter(c => c.querySelector('a'))[0] as Element | null;
-    let user = '';
-    let time_ago = '';
-    let id = '';
-    let quality = '';
-    let content = '[deleted]';
-
-    if (levelRow?.hasAttribute('width')) {
-      level = Number(levelRow.getAttribute('width')) / 40;
-    }
-
-    if (metadata) {
-      const userLink = metadata.querySelector('.hnuser');
-      user = userLink?.textContent || '';
-      time_ago = userLink?.nextElementSibling?.textContent?.replace('|', '').trim() || '';
-      id = (metadata?.querySelector('a[href^=item]')?.getAttribute('href')?.match(/\d+/) || [])[0];
-      const commentEl = row.querySelector('.comment');
-      const replyLink = commentEl?.querySelector('a[href^=reply]');
-
-      // Sometimes the markup becomes nice, and 'reply' link is not part of the comments
-      // Remove 'reply' link
-      if (replyLink?.parentElement?.tagName === 'U') {
-        replyLink.parentElement?.remove();
-      } else {
-        replyLink?.remove();
-      }
-
-      const commtextEl = commentEl?.querySelector('.commtext');
-      const classList = commtextEl?.classList;
-      classList?.remove('commtext')
-      quality = classList?.value || '';
-      content = commtextEl?.innerHTML || '[deleted]'
-      content = '<p>' + content;
-
-      // const doc = new DOMParser().parseFromString(content, 'text/html')
-      // for (const p of from(doc.querySelectorAll('p')) as HTMLParagraphElement[]) {
-      //   if (p.textContent?.startsWith('>')) {
-      //     const bq = doc.createElement('blockquote')
-      //     bq.textContent = p.innerHTML.substr(2);
-      //     p.outerHTML = bq.outerHTML;
-      //   }
-      // }
-      // content = doc.toString();
-    }
-
-    comments.push({ id, level, user, time_ago, content, quality, comments: [] });
-  }
-
-  // Comments are not nested yet, this 2nd loop will nest 'em up
-  for (let i = 0, l = comments.length; i < l; i++) {
-    let comment = comments[i];
-    let level = comment.level;
-
-    if (level > 0) {
-      let index = i, parentComment;
-      do {
-        parentComment = comments[--index];
-      } while (parentComment.level >= level);
-      parentComment.comments.push(comment);
-    }
-  }
-
-  // After that, remove the non-nested ones
-  // Q: WHAT?
-  comments = comments.filter((comment) => comment.level === 0);
-
-  return comments;
-};
-
 export interface Poll {
   item: string | undefined,
   points: number,
 }
 
-function nextAll(initialEl?: Element | null, selectors?: string) {
-  const res: Element[] = [];
-  let el = initialEl?.nextElementSibling;
-  while (el) {
-    if (!selectors) res.push(el)
-    else if (el.matches(selectors)) res.push(el)
-    el = el.nextElementSibling
-  }
-  return res;
-}
-
-function has(els: Element[] | NodeListOf<Element> | null, selectors: string) {
-  return els ? from(els).filter(el => !!el.querySelector(selectors)) : []
-}
-
-function notHas(els: Element[] | NodeListOf<Element>, selectors: string) {
-  return from(els).filter(el => !el.querySelector(selectors))
-}
-
-function first(els?: Element[] | NodeListOf<Element>) {
-  return els && els[0];
-}
-function last(els?: Element[] | NodeListOf<Element>) {
-  return els && els[els.length - 1];
-}
-
-const API = 'https://news.ycombinator.com'
 async function getComments(id: number | string): Promise<Post> {
   try {
     const url = new ParamsURL('/item', { id }, API).href;
@@ -196,46 +97,19 @@ async function getComments(id: number | string): Promise<Post> {
 
 export { getComments as comments }
 
-const tryURL = (url: string): URL | null => {
-  try {
-    return new URL(url);
-  } catch {
-    return null;
-  }
-}
-
-// class TextCon {
-//   obj: { [k: string]: string };
-//   prop: string;
-//   value = '';
-//   constructor(obj: { [k: string]: string }, prop: string) {
-//     this.obj = obj;
-//     this.prop = prop;
-//   }
-//   text({ text, lastInTextNode }: { text: string, lastInTextNode: boolean }) {
-//     this.value += text;
-//     if (lastInTextNode) this.obj[this.prop] = this.value;
-//   }
-// }
-
-async function consume(r: Response) {
-  const reader = r.body!.getReader()
-  while (!(await reader.read()).done);
-}
-
 async function comments(response: Response) {
   const post: Partial<Post> = { title: '', user: '', time_ago: '' }
   const comments: Partial<Comment>[] = []
 
   await consume(new HTMLRewriter()
     .on('.fatitem .athing[id]', {
-      element(voteLink) {
-        post.id = (voteLink.getAttribute('id')?.match(/\d+/) || [])[0] || null;
+      element(el) {
+        post.id = Number(el.getAttribute('id'))
       },
     })
     .on('.fatitem .title a.storylink', {
       element(link) {
-        const url = post.url = link?.getAttribute('href');
+        const url = post.url = link?.getAttribute('href') || undefined;
         post.domain = url && tryURL(url)?.hostname;
       },
       text({ text }) { post.title += text },
@@ -285,106 +159,103 @@ async function comments(response: Response) {
 
   delete comments[0]?._stack;
 
-  try {
-    const commentsR = comments.reverse() as Comment[];
+  const commentsR = comments.reverse() as Comment[];
 
-    for (const [i, comment] of commentsR.entries()) {
-      const { level } = comment;
+  for (const [i, comment] of commentsR.entries()) {
+    const { level } = comment;
 
-      if (level > 0) {
-        let index = i, parentComment: Comment;
-        do {
-          parentComment = commentsR[--index];
-        } while (parentComment.level >= level);
-        parentComment.comments.push(comment);
-      }
+    if (level > 0) {
+      let index = i, parentComment: Comment;
+      do {
+        parentComment = commentsR[--index];
+      } while (parentComment.level >= level);
+      parentComment.comments.push(comment);
     }
+  }
 
-    // After that, remove the non-nested ones
-    // Q: WHAT?
-    post.comments = commentsR.filter(comment => comment.level === 0);
+  // After that, remove the non-nested ones
+  post.comments = commentsR.filter(comment => comment.level === 0);
 
-    return post as Post;
-  } catch (e) { console.error(e.message); throw e }
+  return post as Post;
 };
 
 export interface Post {
-  id?: string | null,
-  title?: string,
-  url?: string | null
-  domain?: string | null,
-  points?: number | null,
-  user?: string | null,
-  time_ago?: string | null,
-  comments_count?: number | null,
-  content?: string | null
+  id: number | null,
+  title: string,
+  url: string
+  domain: string | null,
+  points: number | null,
+  user: string | null,
+  time_ago: string | null,
+  comments_count: number | null,
+  content: string | null
   poll?: Poll[] | null,
-  type?: string,
+  type: string,
   comments: Comment[],
-  more_comments_id: string | null;
+  more_comments_id?: string | null;
 }
 
-export function moreComments(body: string) {
-  if (!/[<>]/.test(body)) {
-    if (/expired/i.test(body)) {
-      throw new Error('Content expired');
-    } else {
-      throw new Error('Not HTML content');
-    }
-  } else {
-    const { document } = parseHTML(body);
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
+// export function moreComments(body: string) {
+//   if (!/[<>]/.test(body)) {
+//     if (/expired/i.test(body)) {
+//       throw new Error('Content expired');
+//     } else {
+//       throw new Error('Not HTML content');
+//     }
+//   } else {
+//     const { document } = parseHTML(body);
+//     const $ = document.querySelector.bind(document);
+//     const $$ = document.querySelectorAll.bind(document);
 
-    const post: Post = {
-      comments: [],
-      more_comments_id: null,
-    };
+//     const post: Post = {
+//       comments: [],
+//       more_comments_id: null,
+//     };
 
-    const commentRows = $$('table:not(:has(table)):has(.comment)');
-    post.comments = processComments(from(commentRows));
+//     const commentRows = $$('table:not(:has(table)):has(.comment)');
+//     post.comments = processComments(from(commentRows));
 
-    // Check for 'More' comments (Rare case)
-    const more = $('td.title a[href^="/x?"]');
-    // Whatever 'fnid' means
-    let fnidM = more?.getAttribute('href')?.match(/fnid=(\w+)/);
-    if (fnidM) {
-      const fnid = fnidM[1];
-      post.more_comments_id = fnid;
-    }
+//     // Check for 'More' comments (Rare case)
+//     const more = $('td.title a[href^="/x?"]');
+//     // Whatever 'fnid' means
+//     let fnidM = more?.getAttribute('href')?.match(/fnid=(\w+)/);
+//     if (fnidM) {
+//       const fnid = fnidM[1];
+//       post.more_comments_id = fnid;
+//     }
 
-    return post;
-  }
-};
+//     return post;
+//   }
+// };
 
-export function newComments(body: string) {
-  if (!/[<>]/.test(body)) {
-    throw new Error('Not HTML content');
-  } else {
-    const { document } = parseHTML(body);
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
+// export function newComments(body: string) {
+//   if (!/[<>]/.test(body)) {
+//     throw new Error('Not HTML content');
+//   } else {
+//     const { document } = parseHTML(body);
+//     const $ = document.querySelector.bind(document);
+//     const $$ = document.querySelectorAll.bind(document);
 
-    const commentRows = $$('tr:nth-child(3) tr:has(.comment)');
-    return processComments(from(commentRows));
-  }
-};
+//     const commentRows = $$('tr:nth-child(3) tr:has(.comment)');
+//     return processComments(from(commentRows));
+//   }
+// };
 
-export function user(body: string) {
-  if (!/[<>]/.test(body)) {
-    throw new Error('Not HTML content');
-  } else {
-    const { document } = parseHTML(body);
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
+// export function user(body: string) {
+//   if (!/[<>]/.test(body)) {
+//     throw new Error('Not HTML content');
+//   } else {
+//     const { document } = parseHTML(body);
+//     const $ = document.querySelector.bind(document);
+//     const $$ = document.querySelectorAll.bind(document);
 
-    const cells = $$('form tr td:odd');
-    const id = cells[0].textContent;
-    const created = cells[1].textContent;
-    const karma = Number(cells[2].textContent);
-    const avg = Number(cells[3].textContent);
-    const about = cleanContent(cells[4].innerHTML);
+//     const cells = $$('form tr td:odd');
+//     const id = cells[0].textContent;
+//     const created = cells[1].textContent;
+//     const karma = Number(cells[2].textContent);
+//     const avg = Number(cells[3].textContent);
+//     const about = cleanContent(cells[4].innerHTML);
 
-    return { id, created, karma, avg, about };
-  }
-}
+//     return { id, created, karma, avg, about };
+//   }
+// }
--- src/routes/news.ts
+++ src/routes/news.ts
@@ -5,6 +5,7 @@ import { formatDistanceToNowStrict } from 'date-fns';
 import { RouteArgs, router } from "../router";
 import { page } from './components';
 import { api } from './api';
+import { Post, stories } from './apidom'
 
 const tryURL = (url: string): URL | null => {
   try {
@@ -61,6 +62,47 @@ const rowEl = (arg: any) => {
     <tr class="spacer" style="height:5px"></tr>`;
 }
 
+export const aThing2 = ({ id, url, title }: Post, index = 0) => {
+  return html`
+    <tr class="athing" id="${id}">
+      <td align="right" valign="top" class="title"><span class="rank">${index != null ? `${index + 1}.` : ''}</span></td>
+      <td valign="top" class="votelinks">
+        <center><a id="up_${id}" onclick="return vote(event, this, &quot;up&quot;)"
+            href="vote?id=${id}&amp;how=up&amp;auth=${'TODO'}&amp;goto=news">
+            <div class="votearrow" title="upvote"></div>
+          </a></center>
+      </td>
+      <td class="title"><a href="${url}"
+          class="storylink">${title}</a><span
+          class="sitebit comhead"> (<a href="from?site=${tryURL(url)?.hostname}"><span
+              class="sitestr">${stripWWW(tryURL(url)?.hostname)}</span></a>)</span></td>
+    </tr>`;
+}
+
+const subtext2 = ({ id, time_ago, points: score, user: by, comments_count: descendants }: Post) => {
+  return html`
+    <tr>
+      <td colspan="2"></td>
+      <td class="subtext">
+        <span class="score" id="score_${id}">${score} points</span> by <a
+          href="user?id=${by}" class="hnuser">${by}</a> <span class="age"><a
+            href="item?id=${id}">${time_ago}</a></span>
+        <span id="unv_${id}"></span>
+        | <a href="hide?id=${id}&amp;auth=${'TODO'}&amp;goto=news" onclick="return hidestory(event, this, ${id})">hide</a> 
+        | <a href="item?id=${id}">${descendants}&nbsp;comments</a></td>
+    </tr>
+  `;
+}
+
+const rowEl2 = (arg: Post, i: number) => {
+  // FIXME: support other types
+  if (arg.type !== 'story') return;
+  return html`
+    ${aThing2(arg, i)}
+    ${subtext2(arg)}
+    <tr class="spacer" style="height:5px"></tr>`;
+}
+
 export function news({ searchParams }: RouteArgs) {
   const p = Number(searchParams.get('p') || '1');
   if (p > Math.ceil(500 / 30)) return notFound('Not supported by Edge HN');
@@ -72,14 +114,8 @@ export function news({ searchParams }: RouteArgs) {
         <table border="0" cellpadding="0" cellspacing="0" class="itemlist">
           <tbody>
             ${async () => {
-              const ids = await api('/v0/topstories.json') as any[];
-              return ids.slice((p - 1) * 30, p * 30).map(async (id, index) => {
-                const item = await api(`/v0/item/${id}.json`);
-                return rowEl({ 
-                  ...item, 
-                  index: (p - 1) * 30 + index,
-                })
-              });
+              const posts = await stories(p);
+              return posts.map(rowEl2);
             }}
             <tr class="morespace" style="height:10px"></tr>
             <tr>
