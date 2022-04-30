3fd5b57a9bc222f983834124d0c2500a198ac662
Florian Klampfer <mail@qwtel.com>
2022-04-30 10:49:29 +0700

    Commit

--- src/entry/deno.ts
+++ src/entry/deno.ts
@@ -1,6 +1,5 @@
 // @ts-ignore
 import { serve } from "https://deno.land/std@0.136.0/http/server.ts";
-// import '@worker-tools/deno-fetch-event-adapter/listen.ts';
 import './html-rewriter-polyfill-deno'
 import { router } from '../routes/index';
-serve(router.serveCallback, { port: Number(self.location.port ?? 8787) });
+await serve(router.serveCallback);
