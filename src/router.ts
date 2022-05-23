1d020b6ff91148b62eaf6371869303e0bac1106e
Florian Klampfer <mail@qwtel.com>
2022-05-23 12:17:57 +0700

    Commit

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { basics, combine, contentTypes, Context } from '@worker-tools/middleware';
 import { WorkerRouter, Method } from '@worker-tools/router';
 
-export const mw = combine(basics(), contentTypes(['text/html', 'application/json']))
+export const mw = combine(basics(), contentTypes(['text/html', 'application/json', '*/*']))
 
 export type RouteArgs = Awaited<ReturnType<typeof mw>>
 
